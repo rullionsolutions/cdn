f57390de19c0e10473082cc5bcf276d069a59862
Joel Kurola <joel.kurola@rullion.co.uk>
2016-10-18 16:34:29 +0100

    styling

--- rullion/master.js
+++ rullion/master.js
@@ -17,29 +17,34 @@
  */
 
 if (!Array.prototype.some) {
-  Array.prototype.some = function(fun/*, thisArg*/) {
-    'use strict';
+    Array.prototype.some = function (fun /*, thisArg*/) {
+        "use strict";
 
-    if (this == null) {
-      throw new TypeError('Array.prototype.some called on null or undefined');
-    }
+        var t;
+        var len;
+        var thisArg;
+        var i;
 
-    if (typeof fun !== 'function') {
-      throw new TypeError();
-    }
+        if (this == null) {
+            throw new TypeError("Array.prototype.some called on null or undefined");
+        }
 
-    var t = Object(this);
-    var len = t.length >>> 0;
+        if (typeof fun !== "function") {
+            throw new TypeError();
+        }
 
-    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-    for (var i = 0; i < len; i++) {
-      if (i in t && fun.call(thisArg, t[i], i, t)) {
-        return true;
-      }
-    }
+        t = Object(this);
+        len = t.length >>> 0;
 
-    return false;
-  };
+        thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+        for (i = 0; i < len; i += 1) {
+            if (i in t && fun.call(thisArg, t[i], i, t)) {
+                return true;
+            }
+        }
+
+        return false;
+    };
 }
 
 var y = {},
